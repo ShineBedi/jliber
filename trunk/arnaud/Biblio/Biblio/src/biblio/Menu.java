99ddad01fb5efe13a418d36ba72fa0c6e296b8e5
kutioo <kutioo@72a5cf40-b6f6-11dd-8584-61fb05dc49a7>
2008-12-09 16:38:51 +0000

    ajout menu

--- trunk/arnaud/Biblio/Biblio/src/biblio/Menu.java
+++ trunk/arnaud/Biblio/Biblio/src/biblio/Menu.java
@@ -33,23 +33,107 @@ public class Menu extends javax.swing.JFrame {
 
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem15 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
-        jMenu4 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenu7 = new javax.swing.JMenu();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
+        jMenuItem11 = new javax.swing.JMenuItem();
+        jMenuItem12 = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenu8 = new javax.swing.JMenu();
+        jMenuItem10 = new javax.swing.JMenuItem();
+        jMenu5 = new javax.swing.JMenu();
+        jMenuItem14 = new javax.swing.JMenuItem();
+        jMenuItem13 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jMenu1.setText("File");
+        jMenu1.setText("Fichier");
+
+        jMenuItem15.setText("Quitter");
+        jMenu1.add(jMenuItem15);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Lecteur");
+
+        jMenuItem1.setText("Nouveau");
+        jMenu2.add(jMenuItem1);
+
+        jMenuItem2.setText("Consulter");
+        jMenu2.add(jMenuItem2);
+
         jMenuBar1.add(jMenu2);
 
+        jMenu3.setText("Ouvrage");
+
+        jMenu6.setText("Exemplaire");
+
+        jMenuItem5.setText("Nouvel");
+        jMenu6.add(jMenuItem5);
+
+        jMenuItem6.setText("Consulter");
+        jMenu6.add(jMenuItem6);
+
+        jMenu7.setText("Statut");
+
+        jMenuItem7.setText("Rendre indisponible");
+        jMenu7.add(jMenuItem7);
+
+        jMenuItem8.setText("Rendre consultable");
+        jMenu7.add(jMenuItem8);
+
+        jMenuItem9.setText("Rendre empruntable");
+        jMenu7.add(jMenuItem9);
+
+        jMenu6.add(jMenu7);
+
+        jMenuItem11.setText("Emprunt");
+        jMenu6.add(jMenuItem11);
+
+        jMenuItem12.setText("Retour");
+        jMenu6.add(jMenuItem12);
+
+        jMenu3.add(jMenu6);
+
+        jMenuBar1.add(jMenu3);
+
         jMenu4.setText("Periodique");
+
+        jMenuItem3.setText("Nouveau");
+        jMenu4.add(jMenuItem3);
+
+        jMenuItem4.setText("Consulter");
+        jMenu4.add(jMenuItem4);
+
+        jMenu8.setText("Parution");
+
+        jMenuItem10.setText("Nouvelle");
+        jMenu8.add(jMenuItem10);
+
+        jMenu4.add(jMenu8);
+
         jMenuBar1.add(jMenu4);
 
-        jMenu3.setText("Ouvrage");
-        jMenuBar1.add(jMenu3);
+        jMenu5.setText("Recherche");
+
+        jMenuItem14.setText("Par auteur");
+        jMenu5.add(jMenuItem14);
+
+        jMenuItem13.setText("Par mot-clé");
+        jMenu5.add(jMenuItem13);
+
+        jMenuBar1.add(jMenu5);
 
         setJMenuBar(jMenuBar1);
 
@@ -83,7 +167,26 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenu jMenu8;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem10;
+    private javax.swing.JMenuItem jMenuItem11;
+    private javax.swing.JMenuItem jMenuItem12;
+    private javax.swing.JMenuItem jMenuItem13;
+    private javax.swing.JMenuItem jMenuItem14;
+    private javax.swing.JMenuItem jMenuItem15;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     // End of variables declaration//GEN-END:variables
 
 }
