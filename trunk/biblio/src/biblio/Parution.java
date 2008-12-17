77ec4077b3667f2c6ecc6d56793dc47fddc4bd8c
kutioo <kutioo@72a5cf40-b6f6-11dd-8584-61fb05dc49a7>
2008-12-17 10:20:49 +0000

    modification de la classe parution

--- trunk/biblio/src/biblio/Parution.java
+++ trunk/biblio/src/biblio/Parution.java
@@ -13,17 +13,32 @@ public class Parution
         extends Observable        // pour MVC
         implements Serializable{
 
-// *************************
-//        Attributs
-// *************************
-private String identifiant_parution;
-private Hashtable articles;
+        private String numpar;
+        
+        private Periodique periodique;
 
+        private Set<Article> articles;
 
+        public Parution(String numpar, Periodique periodique) {
+           this.numpar = numpar;
+           this.lierPeriodique(periodique);
+           articles = new HashSet<Article>();
+        }
 
+        public String numpar() { return numpar; }
+        
+        public Periodique tonPeriodique() {return periodique;}
 
+        public Set<Article> tesArticles() {
+            return (Set<Article>)articles;
+        }
 
+        public void lierPeriodique(Periodique periodique) {
+            this.periodique = periodique;
+        }
 
-
+        public void lierArticle(Set<Article> articles) {
+            this.articles = articles;
+        }
 
 }
