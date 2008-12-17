20ddb6b401762994d7dcc18075ffaf4436a3103b
kutioo <kutioo@72a5cf40-b6f6-11dd-8584-61fb05dc49a7>
2008-12-17 09:50:08 +0000

    Ajout de la classe Article

--- biblio/src/biblio/Article.java
+++ biblio/src/biblio/Article.java
@@ -1,24 +1,64 @@
-/**
- *
- * @author Guillaume
- */
-
 package biblio;
 
 import java.io.*;
-//import java.lang.*;
 import java.util.*;
 
 public class Article
-        extends Observable        // pour MVC
-        implements Serializable{
+        extends Observable            // pour MVC
+        implements Serializable {
 
-// *************************
+//**************************
 //        Attributs
-// *************************
+//**************************
+
+
     private String titre;
-    private int numero_page;
-    private Hashtable auteurs;
-    private Hashtable motscles;
+    private int    page;
+
+    private Parution     parution;
+
+    // Attributs d'Association
+    private Set<Auteur>  auteurs;
+    private Set<MotCle>  motsCles;
+
+    public Article(String titre, int page, Parution parution) {
+        this.titre = titre;
+        this.page = page;
+        this.parution = parution;
+        auteurs = new HashSet<Auteur>();
+        motsCles = new HashSet<MotCle>();
+    }
+
+    //**********************************************
+    // Methodes d'acces aux attributs de la classe
+    //**********************************************
+
+    public String titre() { return titre; }
+    public int page() { return page; }
+
+
+    public Set<Auteur> tesAuteurs() {
+        return (Set<Auteur>)auteurs;
+    }
+
+    public Set<MotCle> tesMotsCles() {
+        return (Set<MotCle>)motsCles;
+    }
+
+    public Parution taParution() {
+        return (Parution)parution;
+    }
+
+
+    // ***************************
+    //     Méthodes privées
+    // ***************************
+    private void lierAuteur(Set<Auteur> auteurs) {
+        this.auteurs = auteurs;
+    } // Fin lierOuvrage
+
+    private void lierMotCle(Set<MotCle> motsCles) {
+        this.motsCles = motsCles;
+    }
 
 }
