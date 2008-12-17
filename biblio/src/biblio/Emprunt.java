f979914f6a997215ed719274cc9c4a6cafde1357
kutioo <kutioo@72a5cf40-b6f6-11dd-8584-61fb05dc49a7>
2008-12-17 10:36:24 +0000

    Ajout de la classe emprunt

---
+++ biblio/src/biblio/Emprunt.java
@@ -0,0 +1,50 @@
+package biblio;
+
+import java.io.*;
+import java.util.*;
+
+public class Emprunt
+        extends Observable            // pour MVC
+        implements Serializable {
+
+//**************************
+//        Attributs
+//**************************
+
+
+    private GregorianCalendar dateEmprunt;
+    private GregorianCalendar dateRetour;
+
+    private Lecteur lecteur;
+    private Exemplaire exemplaire;
+
+    public Emprunt(GregorianCalendar dateEmprunt, GregorianCalendar dateRetour, Lecteur lecteur, Exemplaire exemplaire) {
+        this.dateEmprunt = dateEmprunt;
+        this.dateRetour = dateRetour;
+        this.lierLecteur(lecteur);
+        this.lierExemplaire(exemplaire);
+    }
+
+    //**********************************************
+    // Methodes d'acces aux attributs de la classe
+    //**********************************************
+
+    public GregorianCalendar dateEmprunt() { return dateEmprunt; }
+    public GregorianCalendar dateRetour() { return dateRetour; }
+
+    public Lecteur tonLecteur() {
+        return (Lecteur)lecteur;
+    }
+
+    private void lierLecteur(Lecteur lecteur) {
+        this.lecteur = lecteur;
+    }
+
+    public Exemplaire tonExemplaire() {
+        return (Exemplaire)exemplaire;
+    }
+
+    private void lierExemplaire(Exemplaire exemplaire) {
+        this.exemplaire = exemplaire;
+    }
+}
